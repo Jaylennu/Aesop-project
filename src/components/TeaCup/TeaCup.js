2262704ba7361176b8ec4771c46a869dd84c2ba3
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-05-24 20:57:40 +0900

    REFACTORING: 불필요한 주석 제거 및 파일 삭제

--- src/components/TeaCup/TeaCup.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './TeaCup.scss';
-
-const TeaCup = () => {
-  return <div>Tea Cup</div>;
-};
-
-export default TeaCup;
--- src/components/Teabag/Teabag.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Teabag.scss';
-
-const Teabag = () => {
-  return <div>Teabag</div>;
-};
-
-export default Teabag;
